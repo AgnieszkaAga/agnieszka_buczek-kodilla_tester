b01fdcdbd16cd01165007e916d92b774665fe0d0
Agnieszka Buczek <ak.buczek@gmail.com>
2021-01-18 20:32:31 +0100

    Zadanie 2.8 cwiczymy SWITCH

---
+++ kodilla-intro/src/main/java/org/agnieszka/UserColour/UserColorApp.java
@@ -0,0 +1,8 @@
+package org.agnieszka.UserColour;
+
+public class UserColorApp {
+    public static void main(String[] args){
+        String myColour = UserColour.getUserColour();
+        System.out.println(myColour);
+    }
+}
