8df1844a1aa0ec7afad95f9602d4a2f2a48b0550
Agnieszka Buczek <ak.buczek@gmail.com>
2021-02-20 23:43:47 +0100

    7.2 Zadanie: maszyna do gier

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -1,9 +1,43 @@
 package com.kodilla.parametrized_tests.homework;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GamblingMachineTestSuite {
     GamblingMachine machine = new GamblingMachine();
 
-    // given
+    @ParameterizedTest
+    @CsvFileSource(resources = "/validNumbers.csv")
+    public void returnTrueIfValidNumbersAndResultInRange(String numbers)
+            throws InvalidNumbersException {
+        String[] myStringNumbers = numbers.split(",");
+        Set<Integer> myNumbers = new HashSet<>();
+        for (int n = 0; n< myStringNumbers.length; n++) {
+            Integer a = Integer.parseInt(myStringNumbers[n]);
+            myNumbers.add(a);
+        }
+            int result = machine.howManyWins(myNumbers);
+            assertTrue(result < 7 && result >= 0);
+        }
 
-
-}
+    @ParameterizedTest
+    @CsvFileSource(resources = "/invalidNumbers.csv")
+    public void returnFalseIfNotEnoughOrTooMuchNumbersOrInvalidNumbers(String numbers)
+            throws InvalidNumbersException {
+        String[] myStringNumbers = numbers.split(",");
+        Set<Integer> myNumbers = new HashSet<>();
+        for (int n = 0; n< myStringNumbers.length; n++) {
+            Integer a = Integer.parseInt(myStringNumbers[n]);
+            myNumbers.add(a);
+        }
+        Exception exception = assertThrows(InvalidNumbersException.class, () -> {
+            machine.howManyWins(myNumbers);
+        });
+    }
+    }
