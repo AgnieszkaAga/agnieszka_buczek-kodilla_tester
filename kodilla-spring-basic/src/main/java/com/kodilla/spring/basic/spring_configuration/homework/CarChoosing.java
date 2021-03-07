26de2799de2e5dfe66101ad4161ea123d9c47bae
Agnieszka Buczek <ak.buczek@gmail.com>
2021-03-07 14:14:20 +0100

    9.5 Zadanie: scope beanów

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarChoosing.java
@@ -0,0 +1,22 @@
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+//@Configuration
+//public class CarChoosing {
+//
+//    @Autowired
+//    private String season;
+//
+//    @Bean
+//    public Car createCar(String season) {
+//        Car car;
+//        if (season.equals("summer"))
+//            car = new Cabrio();
+//        else if (season.equals("winter"))
+//            car = new SUV();
+//        else if (season.equals("spring") || season.equals("autumn"))
+//            car = new Sedan();
+//        else
+//            return null;
+//        return car;
+//    }
+//}
---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarChoosingTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarChoosingTestSuite {
+
+//    @Test
+//    public void shouldReturnCorrectCar() {
+//        //Given
+//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+//        CarChoosing carChoosing = context.getBean(CarChoosing.class);
+//        Car car = (Car) context.getBean("createCar");
+//        String seasonBean = context.getBean(String.class);
+//
+//        //When
+//        seasonBean = "winter";
+//        Car myCar = carChoosing.createCar(seasonBean);
+//        boolean lightsOn = myCar.hasHeadlightsTurnedOn(LocalTime.of(21,00));
+//
+//        //Then
+//        Assertions.assertTrue(lightsOn);
+//    }
+}
