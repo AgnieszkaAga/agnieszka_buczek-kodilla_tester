c14942e95e9ef3c859e4e8a80e414db260a6f587
Agnieszka Buczek <ak.buczek@gmail.com>
2021-04-18 19:21:34 +0200

    15.4 Zadanie: tworzenie list zadań - poprawiony import @Test

--- kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -1,9 +1,8 @@
 package com.kodilla.hibernate.tasklist.repository;
 
-import com.kodilla.hibernate.task.repository.TaskRepository;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
