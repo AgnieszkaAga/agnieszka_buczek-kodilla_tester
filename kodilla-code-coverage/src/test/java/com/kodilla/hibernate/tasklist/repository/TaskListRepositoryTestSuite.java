b8ce6671eab1a1c06cc36642aed0b8deedd132ac
Agnieszka Buczek <ak.buczek@gmail.com>
2021-04-18 18:00:18 +0200

    15.4 Zadanie: tworzenie list zadań

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.hibernate.tasklist.repository;
+
+import com.kodilla.hibernate.task.repository.TaskRepository;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListRepositoryTestSuite {
+    @Autowired
+    private TaskListRepository taskListRepository;
+
+    @Test
+    public void testFindByListName(){
+        //Given
+        TaskList taskList = new TaskList("Main task list", "No description for now");
+        taskListRepository.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> result = taskListRepository.findByListName(listName);
+
+        //Then
+        Assert.assertEquals(1, result.size());
+
+        //CleanUp
+        int id = taskList.getId();
+        taskListRepository.deleteById(id);
+    }
+}
